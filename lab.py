f86c4cefa8192f529d7b04286478799db50a4bc7
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2023-10-21 21:17:45 +0400

    Added page scrolling

--- lab.py
+++ lab.py
@@ -23,43 +23,46 @@ def saveImg(imgurl: str, filename: str) -> None:
     except (Exception, requests.exceptions.RequestException) as E:
         print('Ошибка при загрузке: ', imgurl, ':', str(E))
 
+
 def ImgParser(name : str) -> []:
     createFolders(name)
     i = 0
-    url = f"https://yandex.ru/images/search?from=tabbar&text={name}"
-    headers = Headers( 
-        browser="chrome",
-        os="win",
-        headers=True
-        ).generate()
-    html = requests.get(url, headers)
-    print(html)
-    soup = BeautifulSoup(html.content, "lxml")
-    tags = soup.findAll("img",
-                         class_ = "serp-item__thumb justifier__thumb"
-                        )
-    for tag in tags:
-        try:
-            tag = tag.get("src")
-            str_i = str(i)
-            if (i < 10):
-                leading_zeroes = '0' * 3
-            else:
-                if (9 < i < 100):
-                    leading_zeroes = '0' * 2
+    for page in range(20):
+        url = f"https://yandex.ru/images/search?from=tabbar&text={name}&p={page}"
+        headers = Headers( 
+            browser="chrome",
+            os="win",
+            headers=True
+            ).generate()
+        html = requests.get(url, headers)
+        soup = BeautifulSoup(html.content, "lxml")
+        tags = soup.findAll("img",
+                            class_ = "serp-item__thumb justifier__thumb"
+                            )
+        for tag in tags:
+            try:
+                tag = tag.get("src")
+                str_i = str(i)
+                if (i < 10):
+                    leading_zeroes = '0' * 3
                 else:
-                    if (i > 99):
-                        leading_zeroes = '0' * 1
-            formatted_i = leading_zeroes + str_i
-            filename = f"dataset/{name}/{formatted_i}.jpg"
-            saveImg("http:" + tag, filename)
-            i += 1
-            print(i)
-            sleep(1)
-        except Exception as e:
-            print(e)
-            continue
+                    if (9 < i < 100):
+                        leading_zeroes = '0' * 2
+                    else:
+                        if (i > 99):
+                            leading_zeroes = '0' * 1
+                formatted_i = leading_zeroes + str_i
+                filename = f"dataset/{name}/{formatted_i}.jpg"
+                saveImg("http:" + tag, filename)
+                i += 1
+                print(i)
+                sleep(1)
+            except Exception as e:
+                print(e)
+                continue
+
 
 if __name__ == "__main__":
     ImgParser("tiger")
     ImgParser("leopard")
+    
