5b6dd47f0e5db535301dc3f7cd59c80fef011283
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2023-10-24 22:44:00 +0400

    Pictures are not downloaded again, new way to add leading zeros

--- lab.py
+++ lab.py
@@ -5,64 +5,57 @@ from bs4 import BeautifulSoup
 from fake_headers import Headers
 
 
-def createFolders(name:str)->None:
-    if not os.path.exists("dataset"):
-        os.makedirs(f"dataset/{name}")
-    elif  not os.path.exists(f"dataset/{name}"):
-        os.mkdir(f"dataset/{name}")
-
-
-def saveImg(imgurl: str, filename: str) -> None:
+def createFolders(name:str) -> None:
     try:
-        response = requests.get(imgurl, stream=True, timeout=10)
-        if response.status_code == 200:
+        if not os.path.exists("dataset"):
+            os.makedirs(os.path.join("dataset", name))
+        elif  not os.path.exists(os.path.join("dataset", name)):
+            os.mkdir(os.path.join("dataset", name))
+    except Exception:
+        print("Error creating folder!")
+
+
+def saveImg(url: str, filename: str) -> None:
+    try:
+        html = requests.get(url, stream = True, timeout = 10)
+        if html.ok:
             with open(filename, 'wb') as file:
-                for chunk in response.iter_content(1024):
-                    if chunk:
-                        file.write(chunk)
-    except (Exception, requests.exceptions.RequestException) as E:
-        print('Ошибка при загрузке: ', imgurl, ':', str(E))
+                file.write(html.content)
+    except Exception:
+        print(f'Error saving image: {url}')  
 
 
-def ImgParser(name : str) -> []:
+def ImgParser(name : str, url: str) -> None:
     createFolders(name)
-    i = 0
-    for page in range(20):
-        url = f"https://yandex.ru/images/search?from=tabbar&text={name}&p={page}"
-        headers = Headers( 
-            browser="chrome",
-            os="win",
-            headers=True
-            ).generate()
-        html = requests.get(url, headers)
+    list_length = len(os.listdir(os.path.join("dataset", name))) + 2
+    for page in range(list_length // 30, 40):
+        headers = Headers(browser = "Chrome", os = "win", headers = True).generate()
+        html = requests.get(url + f"&p={page}", headers, timeout = 10)
         soup = BeautifulSoup(html.content, "lxml")
-        tags = soup.findAll("img",
-                            class_ = "serp-item__thumb justifier__thumb"
-                            )
-        for tag in tags:
+        images = soup.findAll("img", class_ = "serp-item__thumb justifier__thumb") #возвращает html каждого фото
+        if len(images) == 0:
+            print(soup.text)
+            break
+        for i in range(list_length % 30, len(images)):
             try:
-                tag = tag.get("src")
-                str_i = str(i)
-                if (i < 10):
-                    leading_zeroes = '0' * 3
-                else:
-                    if (9 < i < 100):
-                        leading_zeroes = '0' * 2
-                    else:
-                        if (i > 99):
-                            leading_zeroes = '0' * 1
-                formatted_i = leading_zeroes + str_i
-                filename = f"dataset/{name}/{formatted_i}.jpg"
-                saveImg("http:" + tag, filename)
-                i += 1
-                print(i)
-                sleep(1)
-            except Exception as e:
-                print(e)
+                url_with_tag = images[i].get("src")
+                filename = make_filename(list_length, name)
+                saveImg("http:" + url_with_tag, filename)
+                print(f'Image №: {list_length}')
+                sleep(30)
+                list_length += 1
+                if list_length == 1000:
+                    break
+            except Exception:
+                print(f'Error! Image: {list_length}')
                 continue
 
 
+def make_filename(i: int, name: str) -> str:
+    filename = f"{i:04d}.jpg"
+    return os.path.join("dataset", name, filename)
+
+
 if __name__ == "__main__":
-    ImgParser("tiger")
-    ImgParser("leopard")
-    
+    ImgParser("tiger", "https://www.yandex.ru/images/search?text=tiger")
+    ImgParser("leopard", "https://www.yandex.ru/images/search?text=leopard")
