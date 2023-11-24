66e661e003700fe9e97ea60d6d9adf6a0be56f39
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2023-11-24 14:03:51 +0400

    The application can show the following picture of the classes of the dataset folder. Iterator class changed

---
+++ Instance_Iterator.py
@@ -0,0 +1,30 @@
+import random
+import os
+
+class InstanceIterator:
+
+    def __init__(self, class_label = None, dataset_path = None) -> None:
+        self.class_label = class_label
+        self.dataset_path = dataset_path
+        self.class_path = os.path.join(self.dataset_path, class_label)
+        self.instances = self.get_instances()
+
+
+    def get_instances(self) -> list:
+        if not os.path.exists(self.class_path):
+            print(f"Папка {self.class_label} не найдена.")
+            return None
+
+        instances = os.listdir(self.class_path)
+        random.shuffle(instances)
+        return instances
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> str:
+        if not self.instances:
+            raise StopIteration("Экземпляры закончились.")
+        return os.path.join(self.class_path, self.instances.pop(0))
+
+
--- main_window.py
+++ main_window.py
@@ -1,20 +1,31 @@
-from PyQt5 import QtWidgets
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QVBoxLayout, QLabel, QPushButton, QApplication, QFileDialog
+from PyQt6.QtGui import QPixmap
 from annotation import create_annotation
 from copy1 import copy_dataset  
 from copy_dataset import copy_dataset_with_random_numbers
+from Instance_Iterator import InstanceIterator
 import os
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super(MainWindow, self).__init__()
 
+        self.selected_folder_path = None
+        self.current_image = None 
+        self.resize(300, 100)
         self.folder_path_label = QtWidgets.QLabel('Выберите папку с исходным датасетом:')
         self.folder_path_button = QtWidgets.QPushButton('Выбрать папку', self)
         self.create_annotation_button = QtWidgets.QPushButton('Создать аннотацию', self)
         self.copy_dataset_button = QtWidgets.QPushButton('Создать датасет c файлами типа class_0000.jpg', self)
         self.copy_dataset_with_random_numbers_button = QtWidgets.QPushButton('Создать датасет со случ. названиями файлов', self)
 
-        layout = QtWidgets.QVBoxLayout()
+        layout = QVBoxLayout()
+        self.label = QLabel(self)
+        self.label.setGeometry(0, 0, 100, 100)
+        layout.addWidget(self.label)
+        layout.addWidget(self.button1())
+        layout.addWidget(self.button2())
         layout.addWidget(self.folder_path_label)
         layout.addWidget(self.folder_path_button)
         layout.addWidget(self.create_annotation_button)
@@ -30,37 +41,66 @@ class MainWindow(QtWidgets.QMainWindow):
         self.copy_dataset_button.clicked.connect(self.copy_dataset)
         self.copy_dataset_with_random_numbers_button.clicked.connect(self.copy_dataset_with_random_numbers)
 
-        self.selected_folder_path = None
+    def get_folder_path(self) -> None:
+        """Getting the folder path"""
+        self.selected_folder_path = QFileDialog.getExistingDirectory(self, 'Выберите папку с исходным датасетом')
 
-    def get_folder_path(self):
-        self.selected_folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку с исходным датасетом')
-
-    def create_annotation(self):
+    def create_annotation(self) -> None:
+        """Creating an annotation for a folder"""
         if not self.selected_folder_path:
             return QtWidgets.QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом.')
         relative_path = os.path.relpath(self.selected_folder_path)
         create_annotation(relative_path)
         print(f"Аннотация создана для папки: {relative_path}")
 
-    def copy_dataset(self):
+    def copy_dataset(self) -> None:
+        """Creating a copy of the dataset folder to another folder"""
         if not self.selected_folder_path:
             return QtWidgets.QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом.')
-        new_directory = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку для нового датасета')
+        new_directory = QFileDialog.getExistingDirectory(self, 'Выберите папку для нового датасета')
         relative_path1 = os.path.relpath(self.selected_folder_path)
         relative_path2 = os.path.relpath(new_directory)
         copy_dataset(relative_path1, relative_path2)
         print(f"Датасет скопирован в новую папку: {relative_path2}")
     
-    def copy_dataset_with_random_numbers(self):
+    def copy_dataset_with_random_numbers(self)-> None:
+        """Creating a copy of the dataset folder with a random number of files"""
         if not self.selected_folder_path:
             return QtWidgets.QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом.')
         relative_path = os.path.relpath(self.selected_folder_path)
         copy_dataset_with_random_numbers(relative_path)
-        print(f"Датасет скопирован с случайными номерами в папку: {relative_path}")
+        print(f"Датасет скопирован с случайными номерами в папку: copy_{relative_path}")
 
+    def next_tiger(self) -> None:
+        """Iterator for tiger"""
+        self.class_label = "tiger"
+        instances = InstanceIterator(self.class_label, self.selected_folder_path)
+        image_path = instances.__next__()
+        self.current_image = QPixmap(image_path)
+        self.label.setPixmap(self.current_image.scaled(400, 400))
+
+    def next_leopard(self) -> None:
+        """Iterator for leopard"""
+        self.class_label = "leopard"
+        instances = InstanceIterator(self.class_label, self.selected_folder_path)
+        image_path = instances.__next__()
+        self.current_image = QPixmap(image_path)
+        self.label.setPixmap(self.current_image.scaled(400, 400))
+
+    def button1(self) -> QPushButton:
+        """create button1"""
+        button = QPushButton("Следующая картинка tiger")
+        button.clicked.connect(self.next_tiger)
+        return button
+
+    def button2(self) -> QPushButton:
+        """create button2"""
+        button = QPushButton("Следующая картинка leopard")
+        button.clicked.connect(self.next_leopard)
+        return button
+    
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication([])
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    app.exec()
