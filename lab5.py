453b18f0ae4c3e471e2d14b973f2914932e4fbcd
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2023-12-02 16:56:55 +0400

    Added train loop

--- lab5.py
+++ lab5.py
@@ -3,11 +3,13 @@ from typing import Tuple, Any
 import cv2
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, random_split
+from torch.utils.data import Dataset, random_split, DataLoader
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import torchvision
 import torch.nn as nn
+import torch.optim as optim
+import random
 
 class CustomImageDataset(Dataset):
     def __init__(self, path_to_annotation_file: str, transform: Any=None, target_transform: Any=None) -> None:
@@ -31,16 +33,13 @@ class CustomImageDataset(Dataset):
 
         return image, label
 
-# Загрузка данных
-path_to_dataset = "dataset"
-df = pd.read_csv(os.path.join(path_to_dataset, "annotation3.csv"), header=None, names=['path_to_image', 'label'])
-
 # Пайплайн предобработки данных
 custom_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
                                                     torchvision.transforms.Resize((224, 224)),
                                                     torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
-# Создание объекта датасета
+path_to_dataset = "dataset"
+
 custom_dataset = CustomImageDataset(os.path.join(path_to_dataset, "annotation3.csv"), custom_transforms)
 
 # Разделение данных
@@ -51,34 +50,11 @@ test_size = total_size - train_size - val_size
 
 train_data, val_data, test_data = random_split(custom_dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(42))
 
-# Создание тензоров
-train_labels_tensor = torch.tensor(train_data.dataset.dataset_info['label'].values)
-val_labels_tensor = torch.tensor(val_data.dataset.dataset_info['label'].values)
-test_labels_tensor = torch.tensor(test_data.dataset.dataset_info['label'].values)
-
-# Проверка баланса классов
-train_class_distribution = torch.bincount(train_labels_tensor)
-val_class_distribution = torch.bincount(val_labels_tensor)
-test_class_distribution = torch.bincount(test_labels_tensor)
-
-print("Train Class Distribution:")
-print(train_class_distribution)
-
-print("\nValidation Class Distribution:")
-print(val_class_distribution)
-
-print("\nTest Class Distribution:")
-print(test_class_distribution)
-
-# Визуализация изображений
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.title("tiger" if custom_dataset[0][1] == 0 else "leopard")
-plt.imshow(custom_dataset[0][0].permute(1, 2, 0).numpy()[:, :, ::-1])
-plt.subplot(1, 2, 2)
-plt.title("tiger" if custom_dataset[500][1] == 0 else "leopard")
-plt.imshow(custom_dataset[500][0].permute(1, 2, 0).numpy()[:, :, ::-1])
-plt.show()
+# Загрузчики данных
+batch_size = 32  
+train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 class CNN(nn.Module):
     def __init__(self) -> None:
@@ -115,4 +91,48 @@ class CNN(nn.Module):
         output = self.relu(self.fc1(output))
         output = self.fc2(output)
         return torch.nn.Sigmoid()(output)
-    
+
+# Эксперименты с learning rate и batch size
+learning_rates = [0.001, 0.01, 0.1]  
+batch_sizes = [16, 32, 64]  
+
+for lr in learning_rates:
+    for bs in batch_sizes:
+        print(f"\nTraining with learning rate: {lr}, batch size: {bs}")
+
+        # Инициализация модели, функции потерь и оптимизатора
+        model = CNN()
+        criterion = nn.BCELoss()
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+
+        # Перенос модели на устройство (GPU, если доступен)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+
+        # Цикл обучения
+        num_epochs = 5  
+        for epoch in range(num_epochs):
+            model.train()
+            running_loss = 0.0
+            for inputs, labels in train_dataloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels.float().view(-1, 1))
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+            print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {running_loss / len(train_dataloader):.4f}")
+
+        # Валидация
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for inputs, labels in val_dataloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels.float().view(-1, 1))
+                val_loss += loss.item()
+
+        print(f"Validation Loss: {val_loss / len(val_dataloader):.4f}")
